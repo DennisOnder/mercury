fca41737fd0d94ecf35b011021831049713ddd81
Dennis Onder <webdev.dennisonder@gmail.com>
2018-12-25 21:13:41 +0100

    Landing Page

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="title">
+      <h1>mercury</h1>
+      <h3>crypto-currency chat app.</h3>
     </div>
-    <router-view/>
+    <div class="text-container">
+      <p>...a place where you can chat with crypto-lovers,</p>
+      <p>and check how your favorite cryptocurrency is doing.</p>
+    </div>
+    <div class="buttons">
+      <button>Log In</button>
+      <button>Register</button>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Montserrat");
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)),
+    url("./assets/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  font-family: "Montserrat", Arial, Helvetica, sans-serif;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .title {
+    position: absolute;
+    top: 50px;
+    left: 50px;
+    h1 {
+      font-size: 36px;
+      font-weight: bolder;
+      margin: 0;
+    }
+    h3 {
+      font-size: 28px;
+      font-weight: lighter;
+      margin: 5px 0 0 0;
+    }
+  }
+  .text-container {
+    position: absolute;
+    top: 250px;
+    left: 50px;
+    p {
+      font-size: 20px;
+      margin: 0 0 10px 0;
+    }
+  }
+  .buttons {
+    position: absolute;
+    top: 400px;
+    left: 50px;
+    button {
+      margin: 0 10px 0 0;
+      height: 50px;
+      width: 150px;
+      outline: 0;
+      border: none;
+      background-color: #fff;
+      font-family: "Montserrat", Arial, Helvetica, sans-serif;
+      font-weight: bold;
+      font-size: 16px;
+      border-radius: 50px;
+      cursor: pointer;
+      transition: 0.2s;
+      &:hover {
+        background-color: lighten($color: #9a1641, $amount: 10%);
+        color: #fff;
+      }
     }
   }
 }
